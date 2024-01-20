623943ae785f572462c63e8716bc31cd661ba167
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-20 14:35:06 +0300

    feat: footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import { Typography } from "@mui/material";
+import React from "react";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div
+      style={{
+        position: "fixed",
+        bottom: "0",
+        width: "100vw",
+        height: "60px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor:"greenyellow",
+        zIndex:"2"
+      }}
+    >
+      <Typography>Developed By Mücahit</Typography>
+      <Typography>Copright © Mücahit</Typography>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
