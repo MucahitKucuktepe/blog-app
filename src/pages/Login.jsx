55cf5cba9bfa9a00c1779c3447ece473ff2e3fba
MucahitKucuktepe <mehmet20132204@gmail.com>
2024-01-20 14:57:55 +0300

    feat: router structure added

---
+++ src/pages/Login.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div>Loginlhlgblh</div>
+  )
+}
+
+export default Login
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -3,12 +3,28 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import Dashboard from "../pages/Dashboard";
+import NewBlog from "../pages/NewBlog";
+import About from "../pages/About";
+import Register from "../pages/Register";
+import Login from "../pages/Login";
+import PrivateRouter from "./PrivateRouter";
+import Detail from "../pages/Detail";
+import NotFound from "../pages/NotFound";
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
+        <Route path="newBlog" element={<NewBlog />} />
+        <Route path="about" element={<About />} />
+        <Route path="register" element={<Register />} />
+        <Route path="login" element={<Login />} />
+        <Route path="detail" element={<PrivateRouter />}>
+          <Route path="detail/:id" element={<Detail />} />
+        </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
       <Footer />
     </BrowserRouter>
